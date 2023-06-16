6a0bd5860b9fea721c9842924b6e58f852357c70
Shubham Upadhyay <shubhamup47@gmail.com>
2023-06-16 22:19:04 +0530

    created a submission

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-   var textCase = document.createElement("p");
-   var text = document.createTextNode("DOM load success");
-   textCase.appendChild(text);
-   document.body.appendChild(textCase);
- });
