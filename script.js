24d6d5bbf833ee6177d0f7fa646271ba78131864
Shubham Upadhyay <shubhamup47@gmail.com>
2023-06-16 23:10:01 +0530

    created a submission

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-   var style = document.createElement("style");
-   style.innerHTML = `
-     body:before {
-       content: "DOM load success";
-     }
-   `;
-   document.head.appendChild(style);
-});
